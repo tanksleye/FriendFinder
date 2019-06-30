57201584918b46302eb70dacc2bc4c979e5d4123
Jet <elesebatanksley@yahoo.com>
2019-06-30 18:13:23 -0500

    tried to make the modal work but it is not popping up when the form is submited. new friends are not showing up on the api/friends page

--- app/routes/apiRoutes.js
+++ app/routes/apiRoutes.js
@@ -4,7 +4,7 @@ var friends = require("../data/friends.js");
 module.exports = function(app) {
 
   app.get("/api/friends", function(req, res) {
-     res.json(friendData);
+     res.json(friends);
   });
 
   app.post("/api/friends", function(req, res) {
