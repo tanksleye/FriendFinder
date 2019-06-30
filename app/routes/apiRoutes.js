d44055d976c01de8c6096f46064b9dd770636ca4
Jet <elesebatanksley@yahoo.com>
2019-06-30 16:59:21 -0500

    added friends data, modal, button and answer functions

--- app/routes/apiRoutes.js
+++ app/routes/apiRoutes.js
@@ -1,29 +1,65 @@
 
-var friendData = require("../data/friends");
+var friends = require("../data/friends.js");
 
 module.exports = function(app) {
 
-  app.get("/api/firends", function(req, res) {
+  app.get("/api/friends", function(req, res) {
      res.json(friendData);
   });
 
   app.post("/api/friends", function(req, res) {
-    // req.body hosts is equal to the JSON post sent from the user
-    // This works because of our body parsing middleware
-  if (friendData.length < 1) {
-      friendData.push(req.body);
-      res.json(true);
-  }
-  
-    // Using a RegEx Pattern to remove spaces from newCharacter
-    // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-    // newfriend.routeName = newcharacter.name.replace(/\s+/g, "").toLowerCase();
-  
-    console.log(newfriend);
-  
-    newfriend.push(newfriend);
-  
-    res.json(newfriend);
+    var totalDifference = 0;
+    var bestMatch = {
+      name: "",
+      photo: "",
+      friendDifference: 1000
+    };
+    var userData = req.body;
+    var userName = userData.name;
+    var userScores = userData.scores;
+
+    var b = userScores.map(function(item) {
+      return parseInt(item, 10);
+    });
+    userData = {
+      name: req.body.name,
+      photo: req.body.photo,
+      socres: b
+    };
+
+    console.log("Name: " + userName);
+    console.log("User Score: " + userScores);
+
+    var sum = b.reduce((a, b) => a + b, 0);
+    console.log("Some of user score " + sum);
+    console.log("Best match friend diff " + bestMatch.friendDifference);
+    console.log("********************************************"); 
+
+    for (var i = 0; i < friends.length; i++) {
+      console.log(friends[i].name);
+      totalDifference = 0;
+      console.log("Total Diff " + totalDifference);
+      console.log("Best match friend diff " + bestMatch.friendDifference);
+
+      var bfriendScore = friends[i].scores.reduce((a, b) => a + b, 0);
+      console.log("Total friend score " + bfriendScore);
+      totalDifference += Math.abs(sum - bfriendScore);
+      console.log("-------------------------->" + totalDifference);
+
+      if(totalDifference <= bestMatch.friendDifference) {
+        bestMatch.name = friends[i].name;
+        bestMatch.photo = freinds[i].photo;
+        bestMatch.friendDifference = totalDifference;
+      }
+      console.log(totalDifference + "Total Difference");
+    }
+    console.log(bestMatch);
+    friends.push(userData);
+    console.log("New User Added");
+    console.log(userData);
+    res.json(bestMatch);
   });
 
-}
+
+
+};
--- server.js
+++ server.js
@@ -1,17 +1,24 @@
 
 var express = require("express");
 var path = require("path");
+var bodyParser = require("body-parser");
 // Sets up the Express App
 // =============================================================
 var app = express();
 var PORT = process.env.PORT || 8080;
 // Sets up the Express app to handle data parsing
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
 
+app.use(express.static("app/public"));
 
-require("./app/routes/apiRoutes")(app);
-require("./app/routes/htmlRoutes")(app);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({type: "application/vnd.api+json"}));
+
+require("./app/routes/apiRoutes.js")(app);
+require("./app/routes/htmlRoutes.js")(app);
 
 
   
